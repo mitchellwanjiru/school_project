8b7187c0a45e225daaef9de5c40f6d757ee38ccb
Mitchell <mitchellwanjiru95@gmail.com>
2024-09-28 21:56:02 +0300

    added some text in the homepage and a dashboard page

--- 1_Home.py
+++ 1_Home.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Multipage App"
+    page_title="Learning Assistant App"
 )   
 
 
 
-st.title ("Welcome to Learning Assistant")
+st.title ("Welcome to Learning Assistant App, your new Study Buddy")
+st.text("You can upload your notes, ask questions to the AI, generate quizzes and review flashcards!")
+st.text("Stay organized during the semester, be well prepared for CATs and Exams and pass with flying colours")
---
+++ pages/2_StudyNotes.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.subheader("Upload your notes here")
